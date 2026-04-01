d54b93a9e0843e0d8ab0c534348d876bdd7897a4
Yusuke Shibuya <shibuya@mintclub.net>
2026-04-01 14:37:44 +0900

    Add favicon set and basic PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: [
+        'favicon.ico',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'apple-touch-icon.png',
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png',
+      ],
+      manifest: {
+        name: 'MOSAIC',
+        short_name: 'MOSAIC',
+        description: 'MOSAIC official web app.',
+        start_url: './',
+        display: 'standalone',
+        background_color: '#eff4ff',
+        theme_color: '#cadcff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+      },
+    }),
+  ],
 })
